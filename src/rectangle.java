2636a45f12d048a8d39d39ceea3f6aef7f707b83
Philip Wellener <42978547+philipwellener@users.noreply.github.com>
2018-10-02 14:28:32 -0400

    Add files via upload

---
+++ src/rectangle.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ * @author Philip Wellener
+ * Period 7
+ * Comp Sci
+ *
+ */
+import java.util.Scanner;
+
+
+public class rectangle 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner in = new Scanner(System.in);
+		double xValue = in.nextDouble();
+		in.nextLine();
+		double yValue = in.nextDouble();
+		in.nextLine();
+		
+		double area = xValue*yValue;
+		double perimeter = 2*xValue + 2*yValue;
+		
+		
+
+	}
+
+}
